eae3840c9843bf3d9c0db159c36bf8f8c60fe441
Carlos Peña <corochena@gmail.com>
2019-01-17 18:04:34 -0600

    Rename .py

--- kNN.py
+++ kNN.py
@@ -4,6 +4,8 @@ def distance(p1, p2):
     """Returns the distance between point p1 and point p2"""
     return np.sqrt(np.sum(np.power(p1 - p2, 2)))
     
+    
+    
 p1 = np.array([1,1])
 p2 = np.array([4,4])
 distance(p1, p2)
